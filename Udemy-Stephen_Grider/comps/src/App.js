d3e8a197234206c7fe855ce639675de56a9b81e3
LinMihigo <linmihigo@gmail.com>
2023-11-25 18:57:33 +0200

    Modified: Dropdown.js & Added: Panels.js

--- Udemy-Stephen_Grider/comps/src/App.js
+++ Udemy-Stephen_Grider/comps/src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import Dropdown from "./Components/Dropdown";
 
 function App() {
+    // State that handles the option that was selected
+    const [selection, setSelection] = useState(null);
 
+    // click event handler
+    // Qn. how does option get passed up into the parent element?
+    const handleSelect = (option) => {
+        setSelection(option)
+    }
+
+    // The options to be selected
     const options = [
         {label: 'Red', value: 'red'},
         {label: 'Green', value: 'green'},
         {label: 'Blue', value: 'blue'}
     ]
-    return <Dropdown options={options} />
+    return (
+            <div className='flex'>
+                <Dropdown options={options} value={selection} onChange={handleSelect} />
+                <Dropdown options={options} value={selection} onChange={handleSelect} />
+            </div>
+        )
 }
 
 export default App;
--- Udemy-Stephen_Grider/comps/src/Components/Dropdown.js
+++ Udemy-Stephen_Grider/comps/src/Components/Dropdown.js
@@ -1,30 +1,53 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { GoChevronDown } from "react-icons/go";
+import Panel from './Panel';
 
-function Dropdown({ options }) {
+function Dropdown({ options, value, onChange }) {
+    // State for opening and closing the dropdown list!
     const [isOpen, setIsOpen] = useState(false);
 
+    // Opens the dropdown list when "Select..." is clicked
+    // so that an option can be selected
     const handleClick = () => {
         setIsOpen((currentIsOpen) => !currentIsOpen);
     }
     
+    // Closes the dropdown and shows the option clicked on!
     const handleOptionClick = (option) => {
-        //Close dropdown
+        // Close dropdown
         setIsOpen(false);
-        //Showing the option the user clicked on!
+        // Showing the option the user clicked on!
         console.log(option)
+        // the option selected is passed to the parent component thru this onSelect function
+        onChange(option);
     }
 
+    // enables the options object to be rendered one by one.
     const renderedOptions = options.map((option) => {
         return (
-            <div onClick={() => handleOptionClick(option)} key={option.value}>
+            <div className='hover:bg-sky-100 rounded cursor-pointer p-1' 
+            onClick={() => handleOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         )
-    })
+    });
+
     return (
-        <div>
-            <div onClick={handleClick}>Select...</div>
-            { isOpen && <div>{ renderedOptions }</div> }
+        <div className='w-60 relative'>
+            <Panel className='flex justify-between items-center cursor-pointer' onClick={handleClick}>
+                {/* the ? in selection?. label checks if selection is null and
+                    if it's not, the label will be shown. 
+                    if selection?.label is null, it returns undefined */ }
+                { value?.label || 'Select ...' }
+                <GoChevronDown className='text-lg' />
+            </Panel>
+
+            { /* Options are hidden unless isOpen is true */ }
+            { isOpen && (
+                <Panel className='absolute top-full'>
+                    { renderedOptions }
+                </Panel>) 
+            }
         </div>
     )
 }
