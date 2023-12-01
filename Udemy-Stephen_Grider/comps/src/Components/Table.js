bb11f27a8f069ffe7a3b114c0e936f5da0745dd4
LinMihigo <linmihigo@gmail.com>
2023-12-01 09:01:23 +0200

    Refactor Table.js to make it reusable

--- Udemy-Stephen_Grider/comps/src/Components/Table.js
+++ Udemy-Stephen_Grider/comps/src/Components/Table.js
@@ -1,4 +1,8 @@
-function Table( { data }) {
+function Table( { data, config }) {
+
+    const renderedHeaders = config.map((column) => {
+        return <th key={column.label}>{column.label}</th>
+    });
 
     const renderedRows = data.map((fruit) => {
         return (
@@ -17,9 +21,7 @@ function Table( { data }) {
        <table className="table-auto border-spacing-2">
             <thead>
                 <tr className="border-b-2">
-                    <th>Fruit</th>
-                    <th>Color</th>
-                    <th>Score</th>
+                    { renderedHeaders }
                 </tr>
             </thead>
             <tbody>
