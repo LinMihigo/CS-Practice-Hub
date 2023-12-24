a64ec6488071b2fa1b6f6947f617d24b6c2fb2f5
LinMihigo <linmihigo@gmail.com>
2023-12-24 17:09:24 +0200

    Added cars project

---
+++ Udemy-Stephen_Grider/cars/src/App.js
@@ -0,0 +1,15 @@
+import CarForm from './Components/CarForm';
+import CarList from './Components/CarList';
+import CarSearch from './Components/CarSearch';
+import CarValue from './Components/CarValue';
+
+export default function App() {
+    return (
+        <div>
+            <CarForm />
+            <CarList />
+            <CarSearch />
+            <CarValue />
+        </div>
+    )
+}
---
+++ Udemy-Stephen_Grider/cars/src/Components/CarForm.js
@@ -0,0 +1,4 @@
+export default function CarForm() {
+    return <div>Car form</div>
+    
+}
